e2b2ccd8fdaf7c292ee9f801a0465239645527dc
Akshay Shinde <akshayshinde281195@gmail.com>
2026-02-14 22:53:58 +0530

    Microservice1 source code

---
+++ microservice1/src/main/java/com/example/microservice1/controller/ProductController.java
@@ -0,0 +1,38 @@
+package com.example.microservice1.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.microservice1.entity.Product;
+import com.example.microservice1.service.ProductService;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+	@Autowired
+	private ProductService service;
+	
+	@GetMapping("/products")
+	public List<Product> getProducts() {
+		return service.getAllProducts();
+	}
+	
+	@GetMapping("/products/{id}")
+	public Product getProductById(@PathVariable Long id) {
+		return service.getProductById(id);
+	}
+	
+	@PostMapping("/products")
+	public Product saveProduct(@RequestBody Product product) {
+		return service.saveProduct(product);
+	}
+	
+}
---
+++ microservice1/src/main/java/com/example/microservice1/service/ProductService.java
@@ -0,0 +1,28 @@
+package com.example.microservice1.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.microservice1.entity.Product;
+import com.example.microservice1.repository.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repo;
+	
+	public List<Product> getAllProducts() {
+		return repo.findAll();  // select * from ms1_products
+	}
+	
+	public Product getProductById(Long id) {
+		return repo.findById(id).orElse(null);
+	}
+	
+	public Product saveProduct(Product product) {
+		return repo.save(product);
+	}
+}
